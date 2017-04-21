de27fa0ba4f4273bfd8ef141feed5c47c7977651
Guido Eco <guido.eco@crinstitute.org.uk>
2017-04-21 17:37:51 +0100

    update update.sh file

--- osm3s/update.sh
+++ osm3s/update.sh
@@ -1,4 +1,7 @@
 docker rm osm-server
-mkdir $HOME/data/osm3s/
+for i in archive sync db
+do
+  mkdir -p $HOME/data/osm3s/${i}
+done
 docker run -it -d -v $HOME/data/osm3s/:/opt/osm3s/ --name osm-server --net dockernet --ip 172.18.1.4 -P geco/osm3s:latest
 
